8be5ec5b03420db94ce6a6f9fbb2271b86b0c3db
wang-yuanlongg <1782121679@qq.com>
2022-04-29 08:54:53 +0800

    master second commit

--- gitgit.java
+++ gitgit.java
@@ -2,6 +2,8 @@ public class gitgit {
     public static void main(String[] args) {
         System.out.println("hello git 1");
         System.out.println("hello git 2");
+        System.out.println("hello git 3");
+
 
     }
 }
